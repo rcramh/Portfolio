0bd2a3d8051b50c47600c00a79fdf5cd29c57e7d
rchaubey <rchaubey@micron.com>
2023-06-15 00:04:26 +0530

    adding project details

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -3,6 +3,7 @@ import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
 import rental_bike from "../../Assets/Projects/rental_bike.png";
+import swimato from "../../Assets/Projects/swimato.png";
 import weather from "../../Assets/Projects/weather.png";
 
 function Projects() {
@@ -17,12 +18,25 @@ function Projects() {
           Here are a few projects I've worked on recently.
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+        <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={swimato}
+              isBlog={false}
+              title="Swimato Food orderding"
+              description="Full react based Web app, live Swiggy api integration to fetch restaurant data,  pay for it using integrated payment gateway
+              Tech : ReactJS, ,HTML, CSS ,React Router"
+              ghLink="https://github.com/rcramh/"
+              demoLink="https://google.com/"
+            />
+          </Col>
+
           <Col md={4} className="project-card">
             <ProjectCard
               imgPath={rental_bike}
               isBlog={false}
               title="RentMyBike Web App"
-              description="MERN stack based Web app, User can see, opt for renting a bike of his/her choice, pay for it using integrated payment gateway"
+              description="MERN stack based Web app, User can see, opt for renting a bike of his/her choice, pay for it using integrated payment gateway
+              Tech : ReactJS, ,HTML, CSS ,React Router"
               ghLink="https://github.com/rcramh/"
               demoLink="https://google.com/"
             />
