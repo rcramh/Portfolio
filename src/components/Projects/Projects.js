c1c52ad9577a99b4ec3ffaf8dbb6d18c10915c9a
rchaubey <rchaubey@micron.com>
2023-10-02 12:27:51 +0530

    adding youtube project

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -6,6 +6,7 @@ import rental_bike from "../../Assets/Projects/rental_bike.png";
 import swimato from "../../Assets/Projects/swimato.png";
 import weather from "../../Assets/Projects/weather.png";
 import todoImg from "../../Assets/Projects/todoImg.png";
+import YoutubeImage from "../../Assets/Projects/YoutubeImage.png";
 
 
 function Projects() {
@@ -20,6 +21,18 @@ function Projects() {
           Here are a few projects I've worked on recently.
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+
+        <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={YoutubeImage}
+              isBlog={false}
+              title="Namaste Youtube"
+              description="YouTube clone, User can browser the youtube home page and watch their favorite video,data fetched using live youtube api"
+              ghLink="https://github.com/rcramh/Namaste_Ytb"
+              demoLink="https://youtuberc.vercel.app/"
+            />
+          </Col>
+
         <Col md={4} className="project-card">
             <ProjectCard
               imgPath={swimato}
@@ -27,8 +40,8 @@ function Projects() {
               title="Swimato Food orderding"
               description="Full react based Web app, live Swiggy api integration to fetch restaurant data,  pay for it using integrated payment gateway
               Tech : ReactJS, ,HTML, CSS ,React Router"
-              ghLink="https://github.com/rcramh/"
-              demoLink="https://google.com/"
+              ghLink="https://github.com/rcramh/Swimato_webpack"
+              demoLink="https://swimato-web.vercel.app/"
             />
           </Col>
 
@@ -54,6 +67,8 @@ function Projects() {
               demoLink="https://google.com/"
             />
           </Col>
+
+
 
           <Col md={4} className="project-card">
             <ProjectCard
